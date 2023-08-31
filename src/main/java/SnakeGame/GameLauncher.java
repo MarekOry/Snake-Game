1873ea7d3c32fc203c0e97bc27e225aba05813c5
marek <marek@noreply.com>
2023-08-31 11:42:05 +0200

    define square, add snake and apple

--- src/main/java/SnakeGame/GameLauncher.java
+++ src/main/java/SnakeGame/GameLauncher.java
@@ -10,16 +10,16 @@ public class GameLauncher {
                 int boardWidth = 800;
                 int boardHeight = boardWidth;
 
-                JFrame frame = new JFrame("Snake");
-                frame.setVisible(true);
-                frame.setSize(boardWidth, boardHeight);
-                frame.setLocationRelativeTo(null);
-                frame.setResizable(false);
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                JFrame window = new JFrame("Snake");
+                window.setVisible(true);
+                window.setSize(boardWidth, boardHeight);
+                window.setLocationRelativeTo(null); //centers on screen
+                window.setResizable(false);
+                window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
                 Snake snake = new Snake(boardWidth, boardHeight);
-                frame.add(snake);
-                frame.pack();
+                window.add(snake);
+                window.pack();
             }
         });
     }
